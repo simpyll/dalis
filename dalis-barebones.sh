ab5e8772ae4168613c7ec1352711a933e7b4b97c
David <8688524+simpyll@users.noreply.github.com>
2023-03-24 04:57:35 -0500

    Update dalis-barebones.sh

--- dalis-barebones.sh
+++ dalis-barebones.sh
@@ -1,16 +1,24 @@
-# Install arch linux with as little as possible.
+#! /bin/bash 
+
+# This is an arch Linux install script that is as minimal as possible.
+# © 2023 David Becker 
 #
+# ***LICENSE*** 
+# License: MIT. See it here: https://raw.githubusercontent.com/simpyll/dalis/main/LICENSE
+# 
 # ***IMPORTANT*** 
 # This script assumes you are connected to the internet. 
-# You can do so manually by using an ethernet cable (It is automatic. No scripting or other input needed.) 
+# You can do so directly by using an ethernet cable (It is automatic. No scripting or other input needed.) 
 #
-# Or by using iwctl. Example: 
+# Or by using wifi via iwctl. Example: 
 #
 # iwctl device list
 # iwctl station <stationname> scan
 # iwctl station <stationname> get-networks
 # iwctl station <stationname> connect <ssid> -P <password>
-
+#
+# ***USAGE*** 
+# sh ./dalis-barebones.sh
 
 # Wipe file system and create two new partitions
 sgdisk -Z -a 2048 -o /dev/sda -n 1::+512M -n 2::: -t 1:ef00 
