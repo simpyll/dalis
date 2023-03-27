0e0a1fdac546068f0c8b05c4138e0c1dbd1b0b6f
David <8688524+simpyll@users.noreply.github.com>
2023-03-27 12:58:14 -0500

    Update dalis.sh

--- dalis.sh
+++ dalis.sh
@@ -46,7 +46,7 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 
 # install base packages to mnt
-pacstrap /mnt base base-devel linux linux-firmware intel-ucode networkmanager dhcpcd iwd inetutils iputils efibootmgr vim
+pacstrap /mnt base base-devel linux linux-firmware intel-ucode networkmanager dhcpcd iwd inetutils iputils vim sudo
 
 # generate a partition table 
 genfstab -U /mnt > /mnt/etc/fstab
@@ -80,27 +80,8 @@ arch-chroot /mnt /bin/bash -c 'passwd'
 # generate the ramdisks using the presets inside chroot
 arch-chroot /mnt /bin/bash -c 'mkinitcpio -P'
 
-# Install a bootloader
-bootctl install
-
-# Configure bootloader
-# cat << CONF > /boot/loader/loader.conf
-# default arch
-# timeout 4
-# editor no
-# CONF
-# cat << CONF > /boot/loader/entries/arch.conf
-# title          Arch Linux
-# linux          /vmlinuz-linux
-# initrd         /intel-ucode.img
-# initrd         /initramfs-linux.img
-# options        root=$(blkid | grep sda2 | cut -f 4 -d ' ' | tr -d '"') rw
-# CONF
-
-# create a bootable mount point. 
-arch-chroot /mnt /bin/bash -c 'mkdir /boot/EFI'
-arch-chroot /mnt /bin/bash -c 'mount /dev/sda1 /boot/EFI/'
-arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck'
+arch-chroot /mnt /bin/bash -c 'pacman -S grub efibootmgr'
+arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot'
 arch-chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
 
 # Now we just unmount the filesystem
