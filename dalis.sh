17e3f7ac48f7004d33008e02968ca9c9d3b5c3a5
David <8688524+simpyll@users.noreply.github.com>
2023-03-27 08:53:53 -0500

    Update dalis.sh

--- dalis.sh
+++ dalis.sh
@@ -22,7 +22,7 @@
 # sh dalis-barebones.sh
 
 # wipe file system and create two new partitions (boot and root).
-# sgdisk -Z -a 2048 -o /dev/sda -n 1::+512M -n 2::: -t 1:ef00 
+sgdisk -Z -a 2048 -o /dev/sda -n 1::+512M -n 2::: -t 1:ef00 
 
 # enable NTP
 timedatectl set-ntp true
@@ -31,45 +31,19 @@ timedatectl set-ntp true
 timedatectl set-timezone America/Chicago
 
 # make sda1 a fat32 partition for boot
-# mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sda1
 
 # make a boot directory at /mnt/boot
-# mkdir /mnt/boot 
+mkdir -p /mnt/boot 
 
 # mount sda1 to boot directory
-# mount /dev/sda1 /mnt/boot 
+mount /dev/sda1 /mnt/boot 
 
 # make a ext4 file system for root on sda2
-# mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda2
 
 # mount /dev/sda2 to /mnt
-# mount /dev/sda2 /mnt
-
-# Zap disk
-echo "Zapping disk..."
-sgdisk --zap-all /dev/sda
-
-# Set 1 ESP partition and 1 primary ext4 partition
-parted /dev/sda -s mklabel gpt
-echo "Creating /dev/sda1..."
-parted /dev/sda -s mkpart ESP fat32 1MiB 551MiB
-parted /dev/sda -s set 1 esp on
-echo "Creating /dev/sda2..."
-parted /dev/sda -s mkpart primary ext4 551MiB 100%
-
-# Format the ESP partition as fat32
-echo "Formatting the ESP partition as fat32..."
-yes | mkfs.fat -F32 /dev/sda1
-
-# Format the primary partition as ext4
-echo "Formatting the primary partition as ext4..."
-yes | mkfs.ext4 /dev/sda2
-
-# Mount the partitions
-echo "Mounting partitions..."
 mount /dev/sda2 /mnt
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
 
 # Get available mirrors for the US, and then use rankmirrors to sort them
 echo "Updating mirrorlist..."
@@ -80,9 +54,6 @@ pacstrap /mnt base base-devel linux linux-firmware intel-ucode networkmanager dh
 
 # generate a partition table 
 genfstab -U /mnt > /mnt/etc/fstab
-
-# set locale inside chroot 
-arch-chroot /mnt /bin/bash -c 'echo "en_US.UTF-8" > /etc/locale.gen'
 
 # set keymap inside chroot 
 arch-chroot /mnt /bin/bash -c 'echo "KEYMAP=us" > /etc/vconsole.conf'
@@ -117,28 +88,26 @@ arch-chroot /mnt /bin/bash -c 'mkinitcpio -P'
 bootctl install
 
 # Configure bootloader
-cat << CONF > /boot/loader/loader.conf
-default arch
-timeout 4
-editor no
-CONF
-cat << CONF > /boot/loader/entries/arch.conf
-title          Arch Linux
-linux          /vmlinuz-linux
-initrd         /intel-ucode.img
-initrd         /initramfs-linux.img
-options        root=$(blkid | grep sda2 | cut -f 4 -d ' ' | tr -d '"') rw
-CONF
-
+# cat << CONF > /boot/loader/loader.conf
+# default arch
+# timeout 4
+# editor no
+# CONF
+# cat << CONF > /boot/loader/entries/arch.conf
+# title          Arch Linux
+# linux          /vmlinuz-linux
+# initrd         /intel-ucode.img
+# initrd         /initramfs-linux.img
+# options        root=$(blkid | grep sda2 | cut -f 4 -d ' ' | tr -d '"') rw
+# CONF
 
 # create a bootable mount point. 
-# arch-chroot /mnt /bin/bash -c 'mkdir /boot/EFI'
-# arch-chroot /mnt /bin/bash -c 'mount /dev/sda1 /boot/EFI/'
-# arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck'
-# arch-chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
+arch-chroot /mnt /bin/bash -c 'mkdir /boot/EFI'
+arch-chroot /mnt /bin/bash -c 'mount /dev/sda1 /boot/EFI/'
+arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --bootloader-id=GRUB --recheck'
+arch-chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
 
 # Now we just unmount the filesystem
-
 umount -R /mnt
 
 # all you need to do now is `poweroff` or `restart`. I prefer to poweroff so I can remove the usb without concern before booting back on.
