ac8fcf561e8265dacc592d1d4e220c138c47f15d
David <8688524+simpyll@users.noreply.github.com>
2023-03-27 14:00:37 -0500

    Update dalis.sh

--- dalis.sh
+++ dalis.sh
@@ -81,10 +81,10 @@ arch-chroot /mnt /bin/bash -c 'passwd'
 arch-chroot /mnt /bin/bash -c 'mkinitcpio -P'
 
 # bootloader
-arch-chroot /mnt /bin/bash -c 'pacman -S grub efibootmgr networkmanager dhcpcd iwd inetutils iputils sudo' 
-arch-chroot /mnt /bin/bash -c 'mkdir /boot/EFI'
-arch-chroot /mnt /bin/bash -c 'bootctl install --esp-path /boot/EFI'
-arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/EFI'
+arch-chroot /mnt /bin/bash -c 'pacman -S grub efibootmgr networkmanager dhcpcd iwd inetutils iputils sudo'  
+arch-chroot /mnt /bin/bash -c 'mount /dev/sda2 /boot'
+arch-chroot /mnt /bin/bash -c 'bootctl install --esp-path /boot'
+arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot'
 arch-chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
 
 # Now we just unmount the filesystem
