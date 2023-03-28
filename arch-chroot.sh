1b9e0aeaf46d6f9842754fb7c8b1717dc1e7a671
David <8688524+simpyll@users.noreply.github.com>
2023-03-28 16:55:04 -0500

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -14,7 +14,22 @@ passwd
 
 useradd -m David 
 passwd david
-usermod -aG wheel,audio,video,storage david'
+usermod -aG wheel,audio,video,storage david
 
 # uncomment wheel in visudo
 # vim visudo
+
+pacman -S --noconfirm xorg-server xorg-apps xorg-xinit xdg-user-dirs xorg sudo
+pacman -S --noconfirm i3 i3-gaps i3blocks i3lock numlockx
+pacman -S --noconfirm networkmanager network-manager-applet dhcpcd iw wpa_supplicant dialog openssh
+
+systemctl enable sshd
+systemctl enable dhcpcd
+systemctl enable NetworkManager.service
+
+pacman -S --noconfirm tlp tlp-rdw powertop acpi
+
+systemctl enable tlp 
+systemctl enable tlp-sleep
+systemctl mask systemd-rfkill.service
+systemctl mask systemd-rfkill.socket
