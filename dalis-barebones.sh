38c4dfe749509719b7dc584b3b62a8db6fb424be
David <8688524+simpyll@users.noreply.github.com>
2023-03-28 21:37:35 -0500

    Update dalis-barebones.sh

--- dalis-barebones.sh
+++ dalis-barebones.sh
@@ -17,8 +17,12 @@ arch-chroot /mnt /bin/bash
 
 mkinitcpio -p Linux 
 
-arch-chroot /mnt /bin/bash -c 'pacman -S netctl dialog dhcpcd iw wpa_supplicant'
-arch-chroot /mnt /bin/bash -c 'pacman -S grub efibootmgr'
+arch-chroot /mnt /bin/bash -c 'pacman -S netctl dialog dhcpcd iw wpa_supplicant networkmanager network-manager-applet'
+arch-chroot /mnt /bin/bash -c 'pacman -S grub efibootmgr' 
+
 arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck'
 arch-chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
 arch-chroot /mnt /bin/bash -c 'efibootmgr -d /dev/sda -c -L "Arch Linux" -l vmlinuz-linux -u "root=UUID=$(blkid -s UUID -o value /dev/sda2) rw quiet initrd=intel-ucode.img initrd=initramfs-linux.img"'
+
+arch-chroot /mnt /bin/bash -c 'systemctl enable dhcpcd'
+arch-chroot /mnt /bin/bash -c 'systemctl enable NetworkManager.service'
