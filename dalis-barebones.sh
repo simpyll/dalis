840823a68e638d02cbbc7f11fcf670398ad549c3
David <8688524+simpyll@users.noreply.github.com>
2023-03-28 22:31:33 -0500

    Update dalis-barebones.sh

--- dalis-barebones.sh
+++ dalis-barebones.sh
@@ -2,6 +2,9 @@
 
 sgdisk -Z -a 2048 -o /dev/sda -n 1::+512M -n 2::: -t 1:ef00 
 
+timedatectl set-ntp true
+timedatectl set-timezone America/Chicago
+
 mkfs.fat -F32 /dev/sda1
 yes | mkfs.ext4 /dev/sda2
 
@@ -28,6 +31,22 @@ arch-chroot /mnt /bin/bash -c 'systemctl enable NetworkManager.service'
 arch-chroot /mnt /bin/bash -c 'systemctl start NetworkManager.service' 
 
 arch-chroot /mnt /bin/bash -c 'passwd'
+
+arch-chroot /mnt /bin/bash -c 'echo "KEYMAP=us" > /etc/vconsole.conf'
+arch-chroot /mnt /bin/bash -c 'echo "en_US.UTF-8 UTF-8" > /etc/locale.gen'
+arch-chroot /mnt /bin/bash -c 'ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime'
+arch-chroot /mnt /bin/bash -c 'locale-gen'
+arch-chroot /mnt /bin/bash -c 'hwclock --systohc --utc'
+arch-chroot /mnt /bin/bash -c 'echo "127.0.0.1 localhost
+::1 localhost
+127.0.1.1 arch.localdomain arch" >> /etc/hosts'
+arch-chroot /mnt /bin/bash -c 'echo "archlinux" > /etc/hostname'
+arch-chroot /mnt /bin/bash -c 'hostnamectl set-hostname archlinux'
+
+arch-chroot /mnt /bin/bash -c 'useradd -m david'
+arch-chroot /mnt /bin/bash -c 'passwd david'
+arch-chroot /mnt /bin/bash -c 'usermod -aG wheel,audio,video,storage david'
+
 
 umount -R /mnt 
 poweroff
