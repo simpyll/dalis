0efba3b483a694aea1f4fd73dfc2bf58f53853a6
David <8688524+simpyll@users.noreply.github.com>
2023-03-28 21:31:07 -0500

    Update dalis-barebones.sh

--- dalis-barebones.sh
+++ dalis-barebones.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-cgdisk /dev/sda
+sgdisk -Z -a 2048 -o /dev/sda -n 1::+512M -n 2::: -t 1:ef00 
 
-mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sda1 -L boot
 mkfs.ext4 /dev/sda2 -L root
 
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-pacstrap -i /mnt base base-devel Linux linux-headers
-pacstrap -i /mnt netctl dialog iw wpa_supplicant intel-ucode
+pacstrap -i /mnt base base-devel Linux linux-headers intel-ucode vim
 
 genfstab -p /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash
 
-mkinitcpio -p linux
+mkinitcpio -p Linux 
 
-pacman -S grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
-efibootmgr -d /dev/sda -c -L "Arch Linux" -l vmlinuz-linux -u "root=UUID=$(blkid -s UUID -o value /dev/sda2) rw quiet initrd=intel-ucode.img initrd=initramfs-linux.img"
+arch-chroot /mnt /bin/bash -c 'pacman -S netctl dialog dhcpcd iw wpa_supplicant'
+arch-chroot /mnt /bin/bash -c 'pacman -S grub efibootmgr'
+arch-chroot /mnt /bin/bash -c 'grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck'
+arch-chroot /mnt /bin/bash -c 'grub-mkconfig -o /boot/grub/grub.cfg'
+arch-chroot /mnt /bin/bash -c 'efibootmgr -d /dev/sda -c -L "Arch Linux" -l vmlinuz-linux -u "root=UUID=$(blkid -s UUID -o value /dev/sda2) rw quiet initrd=intel-ucode.img initrd=initramfs-linux.img"'
